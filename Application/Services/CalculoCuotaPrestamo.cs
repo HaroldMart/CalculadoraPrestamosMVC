7bfe595446da43ac5f1d769240562208e8dc7de3
Haroldy <HaroldM09@outlook.com>
2023-03-10 16:54:47 -0400

    Calculadora de prestamos hecha y se organizo el proyecto.

---
+++ Application/Services/CalculoCuotaPrestamo.cs
@@ -0,0 +1,38 @@
+﻿using Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class CalculoCuotaPrestamo
+    {
+        public double prociento { get; set; }
+        public double total { get; set; }
+        public double Cuotamensual { get; set; }
+
+        private CalculoCuotaPrestamo(double porci, double tot, double cuot) {
+
+            this.prociento = porci;
+            this.total = tot;
+            this.Cuotamensual = cuot;
+        }
+
+        public static CalculoCuotaPrestamo CalculoCuota(Prestamos prestamo, int tasaDeInteres)
+        {
+            double monto = prestamo.monto;
+            int meses = prestamo.meses;
+
+            double porciento = (tasaDeInteres * monto) / 100;
+            double total = monto + porciento;
+
+            double cuotaMensual = total / meses;
+
+            CalculoCuotaPrestamo CuotaPago = new(porciento, total, cuotaMensual);
+            return CuotaPago;
+        }
+    }
+}
--- SignoZodiacal/Controllers/PrestamosController.cs
+++ SignoZodiacal/Controllers/PrestamosController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Application.ViewModels;
+using Application.Enum;
+using Application.Services;
 
 namespace CalculadoraPrestamosMVC.Controllers
 {
@@ -6,6 +9,40 @@ namespace CalculadoraPrestamosMVC.Controllers
     {
         public IActionResult Prestamos()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Prestamos(Prestamos prestamo)
+        {
+            int tasaDeInteres;
+            
+            if(prestamo.tipoPrestamo == (int)TipoPrestamosEnum.PERSONAL)
+            {
+                CalculoCuotaPrestamo resultado;
+                tasaDeInteres = 22;
+                resultado = CalculoCuotaPrestamo.CalculoCuota(prestamo, tasaDeInteres);
+                ViewBag.total = resultado.total;
+                ViewBag.cuota = resultado.Cuotamensual;
+
+            } else if (prestamo.tipoPrestamo == (int)TipoPrestamosEnum.AUTOMOVIL)
+            {
+                CalculoCuotaPrestamo resultado;
+                tasaDeInteres = 12;
+                resultado = CalculoCuotaPrestamo.CalculoCuota(prestamo, tasaDeInteres);
+                ViewBag.total = resultado.total;
+                ViewBag.cuota = resultado.Cuotamensual;
+
+            }
+            else if (prestamo.tipoPrestamo == (int)TipoPrestamosEnum.HIPOTECARIO)
+            {
+                CalculoCuotaPrestamo resultado;
+                tasaDeInteres = 8;
+                resultado = CalculoCuotaPrestamo.CalculoCuota(prestamo, tasaDeInteres);
+                ViewBag.total = resultado.total;
+                ViewBag.cuota = resultado.Cuotamensual;
+            }
+
             return View();
         }
     }
