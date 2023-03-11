b7b8ccbf081127d78c562126568d50ccd54326b7
Haroldy <HaroldM09@outlook.com>
2023-03-11 19:30:30 -0400

    Quite el home

--- CalculadoraPrestamosMVC/Program.cs
+++ CalculadoraPrestamosMVC/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Home}/{id?}");
+    pattern: "{controller=Conversor}/{action=Conversor}/{id?}");
 
 app.Run();
